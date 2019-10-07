6c1c725ea9354b8786ecdf88bcad9370e2b26750
bossbobster <34829413+bossbobster@users.noreply.github.com>
2019-10-06 21:10:01 -0700

    Made the code even shorter and more efficient.

--- CowBaseball.cpp
+++ CowBaseball.cpp
@@ -1,5 +1,3 @@
-//solved with elegant, inefficient code (but it works)
-
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -14,8 +12,6 @@ int main()
     sort(places, places + n);
     for(int i = 0; i < n - 2; i ++)
         for(int j = i + 1; j < n - 1; j ++)
-            for(int k = j + 1; k < n; k ++)
-                if(places[k] - places[j] <= (places[j] - places[i]) * 2 && (places[j] - places[i]) <= (places[k] - places[j]))
-                    ans ++;
+            ans += (upper_bound(places, places + n, places[j] + ((places[j] - places[i]) * 2)) - lower_bound(places, places + n, places[j] + (places[j] - places[i]))); 
     cout << ans;
 }
