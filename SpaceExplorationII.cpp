094c6bb84c316bbde875472304499741d8b74f82
bossbobster <34829413+bossbobster@users.noreply.github.com>
2019-10-14 21:56:18 -0700

    I used BFS to do a floodfill and count the different asteroids.

---
+++ SpaceExplorationII.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void solve()
+{
+    struct thing
+    {
+        int row, column;
+    };
+    
+    bool flood[1010][1010];
+    bool visited[1010][1010];
+    char ch;
+    int n, row, column, totalN = 0;
+    queue<thing> nextInLine;
+    cin >> n;
+    for(int i = 0; i <= n + 5; i ++)
+        for(int j = 0; j <= n + 5; j ++)
+        {
+            flood[i][j] = false;
+            visited[i][j] = false;
+        }
+    for(int i = 1; i <= n; i ++)
+    {
+        for(int j = 1; j <= n; j ++)
+        {
+            cin >> ch;
+            if(ch == '*')
+                flood[i][j] = true;
+        }
+    }
+    for(int i = 1; i <= n; i ++)
+    {
+        for(int j = 1; j <= n; j ++)
+        {
+            if(!flood[i][j])
+                continue;
+            nextInLine.push({i, j});
+            do
+            {
+                row = nextInLine.front().row;
+                column = nextInLine.front().column;
+                if(!flood[row][column] || visited[row][column])
+                {
+                    nextInLine.pop();
+                    continue;
+                }
+                if(row == i && column == j)
+                    totalN ++;
+                visited[row][column] = true;
+                nextInLine.push({row, column + 1});
+                nextInLine.push({row, column - 1});
+                nextInLine.push({row + 1, column});
+                nextInLine.push({row - 1, column});
+                nextInLine.pop();
+            }while(!nextInLine.empty());
+        }
+    }
+    cout << totalN;
+}
+
+int main()
+{
+    solve();
+}
