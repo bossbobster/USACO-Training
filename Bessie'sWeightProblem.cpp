6daaac36df6c263648a5aa28a1e685fffe856d3f
bossbobster <34829413+bossbobster@users.noreply.github.com>
2020-07-29 10:43:55 -0700

    Simple DP, very similar to Knapsack.cpp

---
+++ Bessie'sWeightProblem.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool dp[45010];
+int bales[510];
+int main()
+{
+    int n, c, ans = 0;
+    cin >> c >> n;
+    for(int i = 0; i < n; i ++)
+        cin >> bales[i];
+    dp[0] = true;
+    for(int i = 0; i < n; i ++)
+        for(int j = c; j >= 0; j --)
+            if(j - bales[i] >= 0)
+                if(dp[j - bales[i]] >= 0)
+                    dp[j] = max(dp[j], dp[j - bales[i]]);
+    for(int i = c; i >= 0; i --)
+        if(dp[i])
+        {
+            cout << i << "\n";
+            break;
+        }
+}
