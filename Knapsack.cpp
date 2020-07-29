a91c7ea33568d87a7b87d80453e65d40782f90cb
bossbobster <34829413+bossbobster@users.noreply.github.com>
2020-07-29 10:18:46 -0700

    Simple DP on a famous problem.

---
+++ Knapsack.cpp
@@ -0,0 +1,26 @@
+// NOT A USACO PROBLEM
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+typedef pair<int, int> pii;
+
+int dp[5010];
+pii bales[1010];
+int main()
+{
+    int n, c, ans = 0;
+    cin >> n >> c;
+    for(int i = 0; i < n; i ++)
+        cin >> bales[i].first >> bales[i].second;
+    for(int i = 1; i < c; i ++)
+        dp[i] = -2000000000;
+    for(int i = 0; i < n; i ++)
+        for(int j = c; j >= 0; j --)
+            if(j - bales[i].first >= 0)
+                if(dp[j - bales[i].first] >= 0)
+                    dp[j] = max(dp[j], dp[j - bales[i].first] + bales[i].second);
+    for(int i = 0; i <= c; i ++)
+        ans = max(ans, dp[i]);
+    cout << ans << "\n";
+}
