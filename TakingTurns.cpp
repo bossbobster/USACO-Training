19987613c4935a1b6e0d12d6f5d84c404f86111e
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-09-04 21:16:07 -0700

    Simple DP from right to left.

---
+++ TakingTurns.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string.h>
+#include <random>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <complex>
+#include <valarray>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
+typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef pair<float, float> pff;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int nums[700010];
+pll dp[700010];
+int main()
+{
+    input();
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i ++)
+        cin >> nums[i];
+    for(int i = n-1; i >= 0; i --)
+        dp[i] = max(dp[i+1], {dp[i+1].s + nums[i], dp[i+1].f});
+    cout << dp[0].f << ' ' << dp[0].s << '\n';
+}
