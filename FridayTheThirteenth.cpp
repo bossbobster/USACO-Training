6603430f77dfdee449ce1ae31a6d520e00440fdf
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-11-03 17:24:07 -0700

    Even shorter

--- FridayTheThirteenth.cpp
+++ FridayTheThirteenth.cpp
@@ -5,30 +5,22 @@ int days[7];
 int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 int main()
 {
-    int curDay = 0, n;
+    int curDay = 0, n, limit;
     cin >> n;
     for(int i = 1900; i < 1900 + n; i ++)
-    {
         for(int j = 0; j < 12; j ++)
         {
+            limit = month[j];
             if(j == 1 && ((i % 100 != 0 && i % 4 == 0) || (i % 400 == 0)))
-                for(int k = 0; k < 29; k ++)
-                {
-                    if(k == 13)
-                        days[curDay] ++;
-                    curDay ++;
-                    curDay = curDay % 7;
-                }
+                limit = 29;
             else
-                for(int k = 0; k < month[j]; k ++)
+                for(int k = 0; k < limit; k ++)
                 {
                     if(k == 13)
                         days[curDay] ++;
-                    curDay ++;
-                    curDay = curDay % 7;
+                    curDay = (curDay+1) % 7;
                 }
         }
-    }
     for(int i = 0; i < 7; i ++)
         cout << days[(i + 6) % 7] << ((i==6)?'\n':' ');
 }
