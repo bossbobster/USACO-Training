b9c257d1284eb7b5901a07259c5662300e34f71e
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-11-07 11:51:51 -0800

    More accurate solution (USACO test cases bad).

--- SecretCowCode.cpp
+++ SecretCowCode.cpp
@@ -1,25 +1,66 @@
 #include <iostream>
+#include <string.h>
+#include <random>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
 #include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <complex>
+#include <valarray>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
 using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
+typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef pair<float, float> pff;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef pair<ld, ld> pld;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-long long n, mx;
-string s;
+ifstream fin("cowcode.in");
+ofstream fout("cowcode.out");
 
-char rev(long long num)
+ll n, mx;
+string st;
+char F(ll num)
 {
-    if(num <= s.length() && num != 0)
-        return s[num - 1];
-    if(num == 0)
-        return s[num];
-    mx = s.length();
-    while(mx < num)
+    if(num <= st.length())
+        return st[max(0ll, num-1)];
+    mx = st.length();
+    while(mx*2 < num)
         mx *= 2;
-    mx /= 2;
-    return rev(num - mx - 1);
+    if(num == mx+1) return F(num-1);
+    return F(num-mx-1);
 }
-
 int main()
 {
-    cin >> s >> n;
-    cout << rev(n);
+    fin >> st >> n;
+    fout << F(n) << "\n";
 }
