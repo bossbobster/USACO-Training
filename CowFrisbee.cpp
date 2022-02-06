4635cd52f2dc3187dba61f1c78c3ccbe9551063f
BossBobster <34829413+bossbobster@users.noreply.github.com>
2022-02-06 10:20:28 -0800

    Simple monotonic stack solution.

---
+++ CowFrisbee.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string.h>
+#include <random>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <complex>
+#include <valarray>
+using namespace std;
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+//template <class T> using Tree = tree<T, null_type, less<T>,
+//rb_tree_tag, tree_order_statistics_node_update>;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<int, short> pish;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
+typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef pair<float, float> pff;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef pair<ll, ll> pll;
+typedef pair<pll, ll> plll;
+typedef pair<pll, ld> plld;
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef pair<ld, ld> pld;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define eps 1e-9
+//#define cin fin
+//ifstream fin("/Users/bobby/prob1_platinum_feb21/20.in");
+
+int nums[300010];
+stack<pil> cur;
+int main()
+{
+    input();
+    int n;
+    ll ans = 0;
+    cin >> n;
+    for(int i = 0; i < n; i ++)
+        cin >> nums[i];
+    for(ll i = 0; i < n; i ++)
+    {
+        while(!cur.empty() && cur.top().f < nums[i])
+            ans += i-cur.top().s+1, cur.pop();
+        if(!cur.empty())
+            ans += i-cur.top().s+1;
+        cur.push({nums[i], i});
+    }
+    cout << ans << '\n';
+}
