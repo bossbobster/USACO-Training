d1521187171adb5347d64fb018b3c57dd6e9d91b
BossBobster <34829413+bossbobster@users.noreply.github.com>
2022-03-01 18:54:57 -0800

    This is the simpler solution based on some nice parity observations.

---
+++ CountingHaybalesPlat2022.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string.h>
+#include <random>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <complex>
+#include <valarray>
+using namespace std;
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+//template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<int, short> pish;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
+typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef pair<float, float> pff;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef pair<ll, ll> pll;
+typedef pair<pll, ll> plll;
+typedef pair<pll, ld> plld;
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef pair<ld, ld> pld;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define eps 1e-9
+//#define cin fin
+
+const int mod = 1000000007;
+int nums[5010];
+int feas[5010];
+vector<int> idx[2];
+int dp[5010][5010];
+int main()
+{
+    input();
+    int t, n, o, e;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        o = 0; e = 0;
+        for(int i = 0; i <= n+1; i ++)
+            for(int j = 0; j <= n+1; j ++)
+                feas[i] = 0, dp[i][j] = 0;
+        idx[0].clear(); idx[1].clear();
+        idx[0].push_back(0); idx[1].push_back(0);
+        for(int i = 0; i < n; i ++)
+            cin >> nums[i], ((nums[i]&1)?o++:e++), idx[nums[i]&1].push_back(i+1);
+        for(int i = 0; i < n; i ++)
+            for(int j = i+1; j < n; j ++)
+                if(abs(nums[i]-nums[j]) > 2 && (nums[i]&1) != (nums[j]&1))
+                    feas[j+1] = max(feas[j+1], i+1);
+        dp[0][0] = 1;
+        for(int i = 0; i <= o; i ++)
+            for(int j = 0; j <= e; j ++)
+            {
+                if(i+1 <= o && feas[idx[1][i+1]] <= idx[0][j])
+                    dp[i+1][j] = (dp[i+1][j]+dp[i][j]) % mod;
+                if(j+1 <= e && feas[idx[0][j+1]] <= idx[1][i])
+                    dp[i][j+1] = (dp[i][j+1]+dp[i][j]) % mod;
+            }
+        cout << dp[o][e] << '\n';
+    }
+}
